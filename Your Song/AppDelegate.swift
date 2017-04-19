756d99a78a4af6abc8944579c5a1df57787f35f7
tuzmusic <tuzmusic@gmail.com>
2017-04-19 18:59:50 +0200

    *** Before making realm sync work for songs.

--- Your Song/AppDelegate.swift
+++ Your Song/AppDelegate.swift
@@ -14,6 +14,17 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 	
 	var window: UIWindow?
 	
+	/*
+
+	Realm management stuff:
+	1. Delete all songs in local Realm but 3
+	2. Make sure the offline Realm never imports songs
+	-- When no internet, app should only ever show 3 songs --
+	3. Implement update from online realm (when online realm is open, set the config, and refresh results)
+	4. Also have the console print the number of songs in realm to make sure where in the right place (in case we are but the app isn't showing the right things)
+	-- When online, song list should show 3 songs, until realm is opened, then all songs should show --
+	
+	*/
 	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
