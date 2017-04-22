eecdf72ba9fe762f3ccb02112bdc14ba391c8664
tuzmusic <tuzmusic@gmail.com>
2017-04-22 14:04:26 +0200

    Before deleting username and password!

--- Your Song/AppDelegate.swift
+++ Your Song/AppDelegate.swift
@@ -13,38 +13,13 @@ import RealmSwift
 class AppDelegate: UIResponder, UIApplicationDelegate {
 	
 	var window: UIWindow?
-	
-	/*
 
-	Realm management stuff:
-	✔︎ 1. Delete all songs in local Realm but 3
-	✔︎ 2. Make sure the offline Realm never imports songs
-	✔︎ -- When no internet, app should only ever show 3 songs --
-	3. Implement update from online realm (when online realm is open, set the config, and refresh results)
-		Actually, the song count is printed so quickly that the tableview SHOULD populate correctly.
-	
-	Try to solve the schema conflict:
-	✔︎ 1. Delete entire online AND offline Realms
-	✔︎ 2. Run the app. It should recreate and repopulate both realms.
-	3. Do all these steps again.
-	
-	✔︎ (sort of) 4. Also have the console print the number of songs in realm to make sure where in the right place (in case we are but the app isn't showing the right things)
-	-- When online, song list should show 3 songs, until realm is opened, then all songs should show --
-	
-	*** AH HA! It's now saying "3 songs in Online realm" which means it's counting songs from the wrong realm.
-	
-	Once this works (and we know we're accessing the online realm the way we want to):
-	5. Copy songs from online realm to offline realm.
-	6. Run without internet, and full song database should show (can also check in realm browser)
-	
-	*/
-	
 	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
 		// Override point for customization after application launch.
 		
 		//print("Documents folder: \(NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0])")
 		
-		//YpbApp.setupRealm()
+		YpbApp.setupRealm()
 		
 		return true
 	}
--- Your Song/YpbApp.swift
+++ Your Song/YpbApp.swift
@@ -16,7 +16,30 @@ class YpbApp {
 	static var ypbRealm: Realm!
 	
 	class func setupRealm() {
-		// Set the realm configuration
+		/*
+		
+		Realm management stuff:
+		✔︎ 1. Delete all songs in local Realm but 3
+		✔︎ 2. Make sure the offline Realm never imports songs
+		✔︎ -- When no internet, app should only ever show 3 songs --
+		3. Implement update from online realm (when online realm is open, set the config, and refresh results)
+		Actually, the song count is printed so quickly that the tableview SHOULD populate correctly.
+		
+		Try to solve the schema conflict:
+		✔︎ 1. Delete entire online AND offline Realms
+		✔︎ 2. Run the app. It should recreate and repopulate both realms.
+		3. Do all these steps again.
+		
+		✔︎ (sort of) 4. Also have the console print the number of songs in realm to make sure where in the right place (in case we are but the app isn't showing the right things)
+		-- When online, song list should show 3 songs, until realm is opened, then all songs should show --
+		
+		*** AH HA! It's now saying "3 songs in Online realm" which means it's counting songs from the wrong realm.
+		
+		Once this works (and we know we're accessing the online realm the way we want to):
+		5. Copy songs from online realm to offline realm.
+		6. Run without internet, and full song database should show (can also check in realm browser)
+		
+		*/
 		
 		func importSongs() {
 			let importer = SongImporter()
@@ -30,7 +53,13 @@ class YpbApp {
 		let password = "***REMOVED***"
 		let localHTTP = URL(string:"http://54.208.237.32:9080")!
 		
-		SyncUser.logIn(with: .usernamePassword(username: username, password: password), server: localHTTP) {
+		let tokenString = "ewoJImlkZW50aXR5IjogIl9fYXV0aCIsCgkiYWNjZXNzIjogWyJ1cGxvYWQiLCAiZG93bmxvYWQiLCAibWFuYWdlIl0KfQo=:H1qgzZHbRSYdBs0YoJON7ehUZdVDQ8wGKwgYWsQUoupYPycq1cC4PlGZlDZ++Q+gB2ouYcw4bRRri2Z3F5dlWALLWvARgEwB2bDmuOQRcH30IKkdhFp11PnE3StiMn30TDZWWzX31QAyPDvaUyES7/VK/y8CDHmJ8L/UJ/y8w422bmIFTlectnuXBzMRboBZ8JD/PSrXciaPhm9hd/jEEfgYTwB7oyuuch9XrWvPbSrcpWXEr/6j526nuoips1+KTA/h25LzAgCs1+ZeO63RFKi/K3q7y/HkRBB8OWgK9kBQZGIx8eiH4zu7ut4mLGBcs38JnJr4OEvSTSfdZdhGxw=="
+		
+		let tokenCred = SyncCredentials.accessToken(tokenString, identity: "admin")
+		
+		//SyncUser.logIn(with: .usernamePassword(username: username, password: password), server: localHTTP) {
+		SyncUser.logIn(with: tokenCred, server: localHTTP) {
+	
 			
 			// Log in the user. If not, use local Realm config. If unable, return nil.
 			user, error in
