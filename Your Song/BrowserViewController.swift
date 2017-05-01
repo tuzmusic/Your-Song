b8f0e8bd6474003d26f947e79e904ab8deb25811
tuzmusic <tuzmusic@gmail.com>
2017-05-01 18:44:36 +0200

    Before reconfiguring table for alphabetical sections

--- Your Song/BrowserViewController.swift
+++ Your Song/BrowserViewController.swift
@@ -12,22 +12,24 @@ import RealmSearchViewController
 
 class BrowserViewController: RealmSearchViewController {
 	
-
 	override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
 		var activeKeys = [String]()
 		
 		// TO-DO: Doesn't deal with numbers or parentheses yet.
 		let allKeys = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
 		
-		if let results = results {
-			let predString = (entityName == "Song" ? "title" : "name") + " BEGINSWITH %@"
+		if let results = results, let titleName = results.firstObject()?.objectSchema.properties.first?.name {
 			for key in allKeys {
-				if results.objects(with: NSPredicate(format: predString, key)).count > 0 {
+				if results.objects(with: NSPredicate(format: "\(titleName) BEGINSWITH %@", key)).count > 0 {
 					activeKeys.append(key)
 				}
 			}
 		}
 		return activeKeys
+	}
+	
+	override func tableView(_ tableView: UITableView, sectionForSectionIndexTitle title: String, at index: Int) -> Int {
+		<#code#>
 	}
 	
 }
