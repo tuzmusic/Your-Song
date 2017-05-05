6d772f36df2a7778578102acdb928feca0e28737
tuzmusic <tuzmusic@gmail.com>
2017-05-05 21:36:22 +0200

    BrowserViewController very much cleaned up, and functional using adjustedRow(for:)

--- Your Song/BrowserViewController.swift
+++ Your Song/BrowserViewController.swift
@@ -14,23 +14,13 @@ class BrowserViewController: RealmSearchViewController {
 	
 	var activeKeys = [String]()
 	var numberKeyCount = 0
-	//var adjustedRow = 0
 	
-//	var titleName: String? {
-//		return "sortName"
-//	}
-
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Called after 1 call to numberOfSections
-	}
-
 	let numbers = [ "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
 	let letters = [ "A","B","C","D","E","F","G","H","I","J","K","L","M",
 	                "N","O","P","Q","R","S","T","U","V","W","X","Y","Z" ]
 	var allKeys: Array<String> { return numbers + letters }
 
-	func getActiveKeys() {
+	func getActiveKeys() -> [String] {
 		
 		activeKeys.removeAll()
 
@@ -38,20 +28,18 @@ class BrowserViewController: RealmSearchViewController {
 			for key in allKeys {
 				if results.objects(with: NSPredicate(format: "sortName BEGINSWITH %@", key)).count > 0 {
 					if numbers.contains(String(key.characters.first!)), !activeKeys.contains("#") {
-						// If any items start with a number, put the # sign in the index.
-						activeKeys.append("#")
+						activeKeys.append("#") // If any items start with a number, put the # sign in the index.
 					} else {
-						// If the key is a letter, add it to the keys.
-						activeKeys.append(key)
+						activeKeys.append(key) // If the key is a letter, add it to the keys.
 					}
 				}
 			}
 		}
+		return activeKeys
 	}
 	
 	override func numberOfSections(in tableView: UITableView) -> Int {
-		getActiveKeys()
-		return activeKeys.isEmpty ? 1 : activeKeys.count
+		return getActiveKeys().isEmpty ? 1 : activeKeys.count
 	}
 
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -73,30 +61,6 @@ class BrowserViewController: RealmSearchViewController {
 		return Int(results?.count ?? 0)
 	}
 	
-	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-				
-		if results != nil, !activeKeys.isEmpty {
-			if activeKeys.contains("#"), indexPath.section == 0 {
-				for num in 0 ..< numberKeyCount {
-//					return super.tableView.cellForRow(at: IndexPath(row: num, section: 0))!
-					return super.tableView(tableView, cellForRowAt: IndexPath(row: num, section: 0))
-				}
-			}
-//			return super.tableView.cellForRow(at: IndexPath(row: adjustedRow(for: indexPath), section: 0))!
-			return super.tableView(tableView, cellForRowAt: IndexPath(row: adjustedRow(for: indexPath), section: 0))
-			/*
-			let startingLetter = activeKeys[indexPath.section]
-			let items = results.objects(with: NSPredicate(format: "\(titleName) BEGINSWITH[c] %@", startingLetter))
-			let object = items[UInt(indexPath.row)]
-			adjustedRow = Int(results.index(of: object))
-			
-			return super.tableView(tableView, cellForRowAt: IndexPath(row: adjustedRow, section: 0)) */
-			
-		}
-		
-		return super.tableView(tableView, cellForRowAt: indexPath)
-	}
-	
 	func adjustedRow(for indexPath: IndexPath) -> Int {
 		let startingLetter = activeKeys[indexPath.section]
 		if let results = results {
@@ -105,6 +69,11 @@ class BrowserViewController: RealmSearchViewController {
 			return Int(results.index(of: object))
 		}
 		return indexPath.row
+	}
+
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		return super.tableView(tableView, cellForRowAt:
+			(activeKeys.contains("#") && indexPath.section == 0) ? indexPath : IndexPath(row: adjustedRow(for: indexPath), section: 0))
 	}
 	
 	override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
