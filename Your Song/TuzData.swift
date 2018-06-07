10395623ee922e980422f592a173c974bb15b961
tuzmusic <tuzmusic@gmail.com>
2018-06-07 12:44:16 -0400

    added tuzdata file to be ignored

---
+++ Your Song/TuzData.swift
@@ -0,0 +1,13 @@
+//
+//  TuzData.swift
+//  Your Song
+//
+//  Created by Jonathan Tuzman on 6/7/18.
+//  Copyright © 2018 Jonathan Tuzman. All rights reserved.
+//
+
+import Foundation
+
+class TuzData {
+	
+}
