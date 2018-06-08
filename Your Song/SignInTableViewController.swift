7cc55ab17ee3d33f94b1b0e418eec58436f1f63e
Jonathan Tuzman <test@macbook-pro.local>
2018-06-08 16:07:46 -0400

    login failure handled (alert updated)

--- Your Song/SignInTableViewController.swift
+++ Your Song/SignInTableViewController.swift
@@ -12,102 +12,115 @@ import RealmSwift
 import GoogleSignIn
 
 extension YpbUser {
-	class func existingUser (for user: SyncUser, in realm: Realm) -> YpbUser? {
-		let id = user.identity!
-		return realm.objects(YpbUser.self).filter("id = %@", id).first
-	}
+    class func existingUser (for user: SyncUser, in realm: Realm) -> YpbUser? {
+        let id = user.identity!
+        return realm.objects(YpbUser.self).filter("id = %@", id).first
+    }
 }
 
 struct RealmConstants {
-	static let address = "your-piano-bar.us1.cloud.realm.io"
-	static let authURL = URL(string:"https://\(address)")!
-	static let realmURL = URL(string:"realms://\(address)/YourPianoBar/JonathanTuzman/")!
+    static let address = "your-piano-bar.us1.cloud.realm.io"
+    static let authURL = URL(string:"https://\(address)")!
+    static let realmURL = URL(string:"realms://\(address)/YourPianoBar/JonathanTuzman/")!
 }
 
 class SignInTableViewController: UITableViewController, GIDSignInUIDelegate, RealmDelegate {
-	
-	@IBOutlet weak var userNameField: UITextField!
-	@IBOutlet weak var passwordField: UITextField!
-	@IBOutlet var realmLoginButtons: [UIButton]!
-	
-	override func viewWillAppear(_ animated: Bool) {
-		super.viewWillAppear(true)
-		userNameField.text = ""
-		passwordField.text = ""
-	}
-	
-	var spinner = UIActivityIndicatorView()
-	var proposedUser: YpbUser?
-	
-	fileprivate func createNewYpbUser(for info: YpbUser, in realm: Realm) {
-		let newYpbUser = YpbUser.user(id: SyncUser.current!.identity, email: info.email,
-												firstName: info.firstName, lastName: info.lastName)
-		try! realm.write {
-			realm.add(newYpbUser)
-			YpbUser.current = newYpbUser
-			pr("YpbUser created: \(newYpbUser.firstName) \(newYpbUser.lastName) (\(newYpbUser.email)). Set as YpbUser.current.")
-		}
-	}
-	
-	fileprivate func findYpbUser(in realm: Realm) {
-		if let user = YpbUser.existingUser(for: SyncUser.current!, in: realm) { // If we find the YpbUser:
-			try! realm.write { YpbUser.current = user	}
-		} else {
-			pr("YpbUser not found.") // i.e., Realm user found, but not YpbUser. Not sure when this would happen. (Well, it happens when we open an empty realm for some reason)
-			if let info = proposedUser {
-				createNewYpbUser(for: info, in: realm)
-			} else {
-				// we're using a sample user. loginSampleUser doesn't assign a proposed user, so we end up in this else clause, we don't look for or assign YpbUser.current
-				pr("couldn't find creds for sample user")
-			}
-		}
-	}
-	
-	var realm: Realm?
+    
+    @IBOutlet weak var userNameField: UITextField!
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet var realmLoginButtons: [UIButton]!
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        userNameField.text = ""
+        passwordField.text = ""
+    }
+    
+    var spinner = UIActivityIndicatorView()
+    var proposedUser: YpbUser?
+    
+    fileprivate func createNewYpbUser(for info: YpbUser, in realm: Realm) {
+        let newYpbUser = YpbUser.user(id: SyncUser.current!.identity, email: info.email,
+                                      firstName: info.firstName, lastName: info.lastName)
+        try! realm.write {
+            realm.add(newYpbUser)
+            YpbUser.current = newYpbUser
+            pr("YpbUser created: \(newYpbUser.firstName) \(newYpbUser.lastName) (\(newYpbUser.email)). Set as YpbUser.current.")
+        }
+    }
+    
+    fileprivate func findYpbUser(in realm: Realm) {
+        if let user = YpbUser.existingUser(for: SyncUser.current!, in: realm) { // If we find the YpbUser:
+            try! realm.write { YpbUser.current = user	}
+        } else {
+            pr("YpbUser not found.") // i.e., Realm user found, but not YpbUser. Not sure when this would happen. (Well, it happens when we open an empty realm for some reason)
+            if let info = proposedUser {
+                createNewYpbUser(for: info, in: realm)
+            } else {
+                // we're using a sample user. loginSampleUser doesn't assign a proposed user, so we end up in this else clause, we don't look for or assign YpbUser.current
+                pr("couldn't find creds for sample user")
+            }
+        }
+    }
+    
+    var realm: Realm? {
+        didSet {
+            if let realm = realm {
+                // findYpbUser(in: realm)
+                let subscription = realm.objects(Song.self).subscribe()
+                subscriptionToken = subscription.observe(\.state, options: .initial) { state in
+                    if state == .complete {
+                        pr("token state complete, whatever that means")
+                    }
+                }
+                performSegue(withIdentifier: Storyboard.LoginToNewRequestSegue, sender: nil)
+            }
+        }
+    }
     var subscriptionToken: NotificationToken?
     
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		if let user = SyncUser.current {
-			openRealmWithUser(user: user)
-		} else {
-			realm = nil
-		}
-	}
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if let user = SyncUser.current {
+            openRealmWithUser(user: user)
+        } else {
+            realm = nil
+        }
+    }
     
     deinit {
         subscriptionToken?.invalidate()
     }
     
-	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-		spinner.stopAnimating()
-		if let vc = segue.destination as? RegisterTableViewController {	// prepare for Register segue
-			vc.email = userNameField.text
-			vc.password = passwordField.text
-			vc.loginDelegate = self
-		} else if let vc = segue.destination as? CreateRequestTableViewController { // prepare for CreateRequest segue
-			vc.realm = self.realm
-		}
-	}
-		
-	// MARK: Realm-cred sign-in
-	
-	@IBAction func loginButtonTapped(_ sender: UIButton) {
-		if SyncUser.current == nil {
-			if let username = userNameField.text, let password = passwordField.text {
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        spinner.stopAnimating()
+        if let vc = segue.destination as? RegisterTableViewController {	// prepare for Register segue
+            vc.email = userNameField.text
+            vc.password = passwordField.text
+            vc.loginDelegate = self
+        } else if let vc = segue.destination as? CreateRequestTableViewController { // prepare for CreateRequest segue
+            vc.realm = self.realm
+        }
+    }
+    
+    // MARK: Realm-cred sign-in
+    
+    @IBAction func loginButtonTapped(_ sender: UIButton) {
+        if SyncUser.current == nil {
+            if let username = userNameField.text, let password = passwordField.text {
                 guard !password.isEmpty else {
                     nicknameLogin(with: username)
                     return
                 }
-				proposedUser = YpbUser.user(id: nil, email: username, firstName: "", lastName: "") // For the future, we can see if someone with this email already exists, and we can go forward as that YpbUser. This check occurs in the realm.didSet
-				let cred = SyncCredentials.usernamePassword(username: username, password: password)
-				realmCredLogin(cred: cred)
-			}
-		}
-	}
-
+                proposedUser = YpbUser.user(id: nil, email: username, firstName: "", lastName: "") // For the future, we can see if someone with this email already exists, and we can go forward as that YpbUser. This check occurs in the realm.didSet
+                let cred = SyncCredentials.usernamePassword(username: username, password: password)
+                realmCredLogin(cred: cred)
+            }
+        }
+    }
+    
     // YPB Realm login
-
+    
     fileprivate func nicknameLogin(with name: String) {
         let alert = UIAlertController(title: "No password entered",
                                       message: "Do you want to login using \"nickname\" \(name)?", preferredStyle: .alert)
@@ -121,45 +134,39 @@ class SignInTableViewController: UITableViewController, GIDSignInUIDelegate, Rea
         present(alert, animated: true, completion: nil)
     }
     
-	func realmCredLogin(cred: SyncCredentials) {	// Should probably rename, since I think this is for all kinds of creds.
-		
-		spinner = view.addNewSpinner()
-		
-		// Get the user.
-		SyncUser.logIn(with: cred, server: RealmConstants.authURL) { (user, error) in
-			guard let user = user else {
-                self.present(UIAlertController.basic(title: "Uh-Oh", message: "SyncUser.login Error: \(error!)"), animated: true); pr("SyncUser.login Error: \(error as Any)")
-				self.spinner.stopAnimating()
-				// TO-DO: Handle login failure
-				return
-			}
-			pr("SyncUser logged in: \(user)")
-			
-			self.openRealmWithUser(user: user)
-		}
-	}
-	
-	fileprivate func openRealmWithUser(user: SyncUser) {
-			DispatchQueue.main.async { [weak self] in
-				let config = user.configuration(realmURL: RealmConstants.realmURL, fullSynchronization: false, enableSSLValidation: true, urlPrefix: nil)
-                self?.realm = try! Realm(configuration: config)
-                let subscription = self?.realm?.objects(Song.self).subscribe()
-                self?.subscriptionToken = subscription?.observe(\.state, options: .initial) { state in
-                    if state == .complete {
-                        pr("token state complete, whatever that means")
-                    }
-                }
-                // findYpbUser(in: realm)
-                self?.performSegue(withIdentifier: Storyboard.LoginToNewRequestSegue, sender: nil)                
-			}
-	}
+    func realmCredLogin(cred: SyncCredentials) {	// Should probably rename, since I think this is for all kinds of creds.
+        
+        spinner = view.addNewSpinner()
+        
+        // Get the user.
+        SyncUser.logIn(with: cred, server: RealmConstants.authURL) { [weak self] (user, error) in
+            if let user = user {
+                pr("SyncUser logged in: \(user)")
+                self?.openRealmWithUser(user: user)
+            } else if let error = error {
+                self?.handleLogIn(error)
+            }
+        }
+    }
     
-	@IBAction func logOutAll() {
-		SyncUser.current?.logOut()
-		realm = nil
-		proposedUser = nil
-		userNameField.text = ""
-		passwordField.text = ""
-	}
-
+    fileprivate func openRealmWithUser(user: SyncUser) {
+        DispatchQueue.main.async { [weak self] in
+            let config = user.configuration(realmURL: RealmConstants.realmURL, fullSynchronization: false, enableSSLValidation: true, urlPrefix: nil)
+            self?.realm = try! Realm(configuration: config)
+        }
+    }
+    
+    fileprivate func handleLogIn(_ error: Error) {
+        self.present(UIAlertController.basic(title: "Login failed", message: error.localizedDescription), animated: true); pr("SyncUser.login Error: \(error)")
+        self.spinner.stopAnimating()
+    }
+    
+    @IBAction func logOutAll() {
+        SyncUser.current?.logOut()
+        realm = nil
+        proposedUser = nil
+        userNameField.text = ""
+        passwordField.text = ""
+    }
+    
 }
